2ca0964b06631cc4458b0a313fca3ece11a03552
emelinpaola <114231932+emelinpaola@users.noreply.github.com>
2022-10-07 08:19:11 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -7,15 +7,23 @@ int main()
     return 0;
 }
 #include <stdio.h>
-/* Estancia
-El programa, al recibir como dato la superficie de una estancia expresada
-➥en acres, la convierte a hectáreas.
-ECA: variable de tipo real. */
+#include <math.h>
+/* Perímetro del triángulo.
+El programa, al recibir las coordenadas de los puntos P1, P2 y P3 que
+➥corresponden a los vértices de un triángulo, calcula su perímetro.
+X1, Y1, X2, Y2, X3, Y3, LA1, LA2, LA3 y PER: variables de tipo real. */
 void main(void)
 {
-float ECA;
-printf(“Ingrese la extensión de la estancia: “);
-scanf(“%f”, &ECA);
-ECA = ECA * 4047 / 10000;
-printf(“\nExtensión de la estancia en hectáreas: %5.2f”, ECA);
+float X1,Y1,X2,Y2,X3,Y3,LA1,LA2,LA3,PER;
+printf(“Ingrese la coordenada del punto P1:”);
+scanf(“%f %f”, &X1, &Y1 );
+printf(“Ingrese la coordenada del punto P2:”);
+scanf(“%f %f”, &X2, &Y2 );
+printf(“Ingrese la coordenada del punto P3:”);
+scanf(“%f %f”, &X3, &Y3 );
+LA1 = sqrt(pow(X1-X2, 2) + pow(Y1-Y2, 2));
+LA2 = sqrt(pow(X2-X3, 2) + pow(Y2-Y3, 2));
+LA3 = sqrt(pow(X1-X3, 2) + pow(Y1-Y3, 2));
+PER = LA1 + LA2 + LA3;
+printf(“\nEl perímetro del triángulo es: %6.3f”, PER);
 }
