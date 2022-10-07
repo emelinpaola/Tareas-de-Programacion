b5a6ba44536f9fa189da65478c1788de8d703c54
emelinpaola <114231932+emelinpaola@users.noreply.github.com>
2022-10-07 08:12:23 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -10,15 +10,11 @@ int main()
 /* Aplicación de operadores. */
 void main(void)
 {
-int i= 5, j = 7, k = 3, m1;
-float x = 2.5, z = 1.8, t;
-m1 = ((j % k) / 2) + 1;
-m1 += i;
-m1 %= --i;
-printf(“\nEl valor de m1 es: %d”, m1);
-t = ((float) (j % k) / 2);
-t++;
-x *= ++z;
-t -= (x += ++i);
-printf(“\nEl valor de t es: %.2f”, t);
+int i = 15, j, k, l;
+j = (15 > i--) > (14 < ++i);
+printf(“\nEl valor de j es: %d”, j);
+k = ! (‘b’ != ‘d’) > (!i - 1);
+printf(“\nEl valor de k es: %d”, k);
+l = (! (34 > (70 % 2))  0);
+printf(“\nEl valor de l es: %d”, l);
 }
