386cd60a5997c2d190a8540abc9f9dfb80c7f7c8
emelinpaola <114231932+emelinpaola@users.noreply.github.com>
2023-02-04 21:30:11 -0400

    Add files via upload

---
+++ programa fecha de nacimiento/calcular la edad de una persona/calcular la edad de una persona/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio
+{
+     class program
+    {
+        static void main(string[] args)
+        {
+            //Calcular la edad de una persona en C#
+            Console.Write("ingrese el año actual:");
+            int año_actual = int.Parse(Console.ReadLine());
+            Console.Write("ingrese su año de nacimiento:");
+            int año_nacimiento = int.Parse(Console.ReadLine());
+            int edad = año_actual - año_nacimiento;
+            Console.Write("usted tiene" + edad + "años.");
+            Console.ReadKey();*/ 
+
+                int edad, año_nacimiento;
+            Console.Write("ingrese su año de nacimiento: ");
+            año_nacimiento = int.Parse(Console.ReadLine());
+            edad = DateTime time.today.year - año_nacimiento;
+            Console.Write("usted tiene " + edad + "años.");
+            Console.ReadKey();
+
+
+
+
+        }
+
+        
+    }
+}
---
+++ programa fecha de nacimiento/programa fecha de nacimiento/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace programa_fecha_de_nacimiento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.Write("ingrese el año actual:");
+            int año_actual = int.Parse(Console.ReadLine());
+            Console.Write("ingrese su año de nacimiento:");
+            int año_nacimiento = int.Parse(Console.ReadLine());
+            int edad = año_actual - año_nacimiento;
+            Console.Write("usted tiene" + edad + "años.");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
