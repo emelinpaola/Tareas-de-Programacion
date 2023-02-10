d8cf7f5c88bafa961bf997bac37e17d26383d48c
emelinpaola <114231932+emelinpaola@users.noreply.github.com>
2023-02-10 18:02:05 -0400

    Add files via upload

---
+++ edad/edad/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace edad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpNacimineto = new System.Windows.Forms.DateTimePicker();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dtpNacimineto
+            // 
+            this.dtpNacimineto.Location = new System.Drawing.Point(101, 26);
+            this.dtpNacimineto.Name = "dtpNacimineto";
+            this.dtpNacimineto.Size = new System.Drawing.Size(139, 20);
+            this.dtpNacimineto.TabIndex = 1;
+            this.dtpNacimineto.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(125, 155);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(94, 56);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(82, 105);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.ReadOnly = true;
+            this.txtresultado.Size = new System.Drawing.Size(179, 20);
+            this.txtresultado.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 236);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.dtpNacimineto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpNacimineto;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtresultado;
+    }
+}
+
---
+++ edad/edad/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace edad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            DateTime fechadelusuario = dtpNacimineto.Value;
+            DateTime fechadehoy = DateTime.Now;
+            int diferenciaenaños = (fechadehoy.Year - fechadelusuario.Year);
+            if (fechadelusuario > fechadehoy.AddYears(-diferenciaenaños))
+            {
+                diferenciaenaños--;
+            }
+            txtresultado.Text = "Usted tiene " + diferenciaenaños.ToString() + " años";
+        }
+        void calcularDiferencia()
+        {
+            
+        }
+       
+        
+       private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
